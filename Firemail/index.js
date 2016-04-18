bc8cc08eb62964e4e4670192ca1e524728e0a749
hechengjin <hechengjin@kingsoft.cn>
2016-04-19 00:05:45 +0800

    打开带有调试工具的独立窗口发送邮件

--- Firemail/index.js
+++ Firemail/index.js
@@ -1,66 +1,39 @@
 'use strict';
 var $ = require('dombo')
+const assert = require('assert')
+const path = require('path')
+const BrowserWindow = require('electron').remote.BrowserWindow
 
-let nodemailer = require("nodemailer");
 
 let sentMailBrowserWindowProxy;
+let sendBrowserWindow
+var fixtures = path.resolve(__dirname, '')
 function openSendMailDialog () {
-  // alert(111)
-  // console.log('1111')
-  sentMailBrowserWindowProxy = window.open('file://' + __dirname + '/sentMail.html', 'sentMail',
-   'width=500,height=300,skipTaskbar=yes,autoHideMenuBar=yes');
+   sendBrowserWindow = new BrowserWindow({
+      show: true,
+      width: 400,
+      height: 400
+    })
+
+    sendBrowserWindow.webContents.on('did-finish-load', function () {
+        sendBrowserWindow.webContents.openDevTools()
+        try {
+          sendBrowserWindow.webContents.debugger.attach()
+        } catch (err) {
+          assert(sendBrowserWindow.webContents.debugger.isAttached())
+        }
+      })
+      //alert('file://' + path.join(fixtures, '', 'sentMail.html'))
+      sendBrowserWindow.webContents.loadURL('file://' + path.join(fixtures, '', 'sentMail.html'))
 }
 
 
-function sendMail () {
-  let nm = nodemailer.createTransport({
-				host: 'smtp.139.com',
-				port: 25,
-				auth: {
-						user: '15313159857@139.com',
-						pass: 'Draeagi94'
-				},
-				ignoreTLS: true,
-				logger: false
-		});
 
-    let myDate = new Date()
-    let dateString = myDate.toLocaleString()
-    dateString = dateString + '.'
+$('#sendMail').on('click', openSendMailDialog)
 
-    // return getMailInfo(dateString, nm)
-    let count = 0
-    setInterval(() => {
-      getMailInfo(dateString, nm).then(function(info) {
-      console.log('OK: ' + count + ' '+ JSON.stringify(info))
-      }, function(error) {
-        console.error('Error: ' + count + ' '+ error);
-      });
-    }, 5000);
-}
-var getMailInfo = function(dateString, nm) {
-  var promise = new Promise(function(resolve, reject){
-    let myDate = new Date()
-    let milliseconds = myDate.getMilliseconds()
-    let mailData = {
-      from: '15313159857@139.com',
-      to: ['bigdata_fire@hotmail.com'],
-      subject: 'html5学习' + dateString + milliseconds,
-      date: new Date(),
-      messageId: 'www.firemail.wang_' + dateString + milliseconds,
-      xMailer: 'aaa',
-      text: '一起学习Html5，QQ群：245146134'
-    };
-
-    nm.sendMail(mailData, function (err, info) {
-      if (err === null) {
-        resolve(info);
-      } else {
-        reject(err);
-      }
-    });
-  });
-
-  return promise;
-};
-$('#sendMail').on('click', sendMail)
+window.addEventListener('unload', function (e) {
+  if (sendBrowserWindow != null) {
+     sendBrowserWindow.destroy()
+   }
+   sendBrowserWindow = null
+}, false);
--- Firemail/sentMail.js
+++ Firemail/sentMail.js
@@ -1,9 +1,58 @@
 'use strict';
 var $ = require('dombo')
+let nodemailer = require("nodemailer");
 
 
 function sendMail () {
-  
+  let nm = nodemailer.createTransport({
+				host: 'smtp.139.com',
+				port: 25,
+				auth: {
+						user: '15313159857@139.com',
+						pass: 'Draeagi94'
+				},
+				ignoreTLS: true,
+				logger: false
+		});
+
+    let myDate = new Date()
+    let dateString = myDate.toLocaleString()
+    dateString = dateString + '.'
+
+    // return getMailInfo(dateString, nm)
+    let count = 0
+    setInterval(() => {
+      getMailInfo(dateString, nm).then(function(info) {
+      console.log('OK: ' + count + ' '+ JSON.stringify(info))
+      }, function(error) {
+        console.error('Error: ' + count + ' '+ error);
+      });
+    }, 5000);
 }
+var getMailInfo = function(dateString, nm) {
+  var promise = new Promise(function(resolve, reject){
+    let myDate = new Date()
+    let milliseconds = myDate.getMilliseconds()
+    let mailData = {
+      from: '15313159857@139.com',
+      to: ['bigdata_fire@hotmail.com'],
+      subject: 'html5学习' + dateString + milliseconds,
+      date: new Date(),
+      messageId: 'www.firemail.wang_' + dateString + milliseconds,
+      xMailer: 'aaa',
+      text: '一起学习Html5，QQ群：245146134'
+    };
+
+    nm.sendMail(mailData, function (err, info) {
+      if (err === null) {
+        resolve(info);
+      } else {
+        reject(err);
+      }
+    });
+  });
+
+  return promise;
+};
 
 $('#sendMail').on('click', sendMail)
